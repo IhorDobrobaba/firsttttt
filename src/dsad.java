a15d31060097c1dd74f13eb614f6c30ee8662ad1
IhorDobrobaba <dobrobov@gmail.com>
2018-11-10 14:15:48 +0200

    ddasdaddsadasd1423412dsad

--- src/dsad.java
+++ src/dsad.java
@@ -1,17 +1,7 @@
 public class dsad {
     public static void main (String[] args){{
        //dsadas
-       //dsadas
-       //dsadas
-       //dsadas
-       //dsadas
-       //dsadas
-       //dsadas
-       //dsadas
-       //dsadas
-       //dsadas
-        //jjj
-        //dsadasdas
+       
     }
     }
 }
